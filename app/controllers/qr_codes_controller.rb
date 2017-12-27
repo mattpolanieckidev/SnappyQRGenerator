7cae8939e7fbf293f31dfa75b5e014a0357e37dc
matpol88 <matpol88@gmail.com>
2017-12-27 09:37:23 -0500

    Tried PNG

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -3,7 +3,21 @@ class QrCodesController < ApplicationController
   end
 
   def create
-    @qr = RQRCode::QRCode.new( qr_code_params[:text], size: 4)
+    
+    @qr = RQRCode::QRCode.new(qr_code_params[:text], size: 4)
+# With default options specified explicitly
+png = qrcode.as_png(
+          resize_gte_to: false,
+          resize_exactly_to: false,
+          fill: 'white',
+          color: 'black',
+          size: 120,
+          border_modules: 4,
+          module_px_size: 6,
+          file: nil # path to write
+          )
+IO.write("/tmp/github-qrcode.png", png.to_s)
+    
   end
 
 private
